2c583398b0840fe77b363fba54fbdfa464a48d5e
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-02-16 16:08:16 +0530

    16-02-26

---
+++ PYTHON/041_file.py
@@ -0,0 +1,12 @@
+input_string = "aaabbc"
+dict = {}
+
+for i in input_string:
+    dict[i] = dict.get(i, 0) + 1
+
+res = ""
+
+for k,v in dict.items():
+    res = res + (k+str(v))
+
+print(res)  #output :  a3b2c1
