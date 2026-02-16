c75714b13cc2ea85427b5e345e031934457e9a4c
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-02-16 21:43:36 +0530

    16-02-26

--- Leetcode/GroupAnagram.py
+++ Leetcode/GroupAnagram.py
@@ -1,12 +1,14 @@
+#Group Anagrams
 strs = ["eat","tea","tan","ate","nat","bat"]
 #output :  [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
 
 wordfreq = [0] * 26
-groupfreq = {}
+group_freq = {}
 for word in strs:
     for ch in word:
         wordfreq[ord(ch) - ord('a')] += 1
-    groupfreq[str(wordfreq)] = groupfreq.get(str(wordfreq), []) + [word]
+    key = str(wordfreq)
+    group_freq[key] = group_freq.get(key, []) + [word]
     wordfreq = [0] * 26 # reset
 
-print(list(groupfreq.values()))
+print(list(group_freq.values()))
