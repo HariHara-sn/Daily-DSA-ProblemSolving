a29f30e572f4a7b67d4d99a3e6a6af5bfc502b36
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-03-18 10:37:30 +0530

    18-03-26

---
+++ PYTHON/050_file.py
@@ -0,0 +1,24 @@
+#First unique character
+#method-1
+name = "priyabharathi"
+for i in name:
+    if name.index(i) == name.rindex(i):
+        print(i, "  ", name.index(i))
+        break
+
+#method-2
+name = "hariharasudhan"
+for ch in name:
+    if name.count(ch) == 1:
+        print(ch, "  ", name.index(ch))
+        break
+
+#method-3
+name = "priyabharathi"
+freq = {}
+for ch in name:
+    freq[ch] = freq.get(ch, 0) + 1
+for i in name:
+    if freq[i] == 1:
+        print(i, "  ", name.index(i))
+        break
