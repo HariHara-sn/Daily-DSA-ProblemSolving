50e75d985794ab33b9ae0f40f8ffe39fa9c52fd0
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-03-22 18:56:24 +0530

    22-03-26

--- PYTHON/051_file.py
+++ PYTHON/051_file.py
@@ -1,8 +1,5 @@
-input_digit = 10 
-binary = format(input_digit, "b")
-binary = str(binary)
-print(binary)
-
-res = "".join('1' if bit == '0' else '0' for bit in binary)
-
-print(int(res, 2))
+# to find percentage of a number
+num = 1200
+per = 10
+result = (num * per) // 100
+print(result)
