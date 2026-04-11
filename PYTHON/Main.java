ecfb8ba4453f3047205fe3cf2b502ebb7c05f3e2
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-04-11 15:46:57 +0530

    reverse - arr list

--- PYTHON/Main.java
+++ PYTHON/Main.java
@@ -1,9 +1,15 @@
 
 //ctrl + ` = focus terminal
 // In python You can use list into set, only tuples are allowed in set
-public class Main {
-
-    public static void main(String[] args) {
-        
-    }    
+public class Main
+{
+    public static boolean suma(String token) {
+        return token != null; 
+	    
+    }
+	public static void main(String[] args) {
+	    String token = "aifan2r3i03uapc4u39qusv33u";
+	    
+		System.out.println(suma(token));
+	}
 }
