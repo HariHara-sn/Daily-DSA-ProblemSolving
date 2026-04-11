bffd2658284699178a1b7848e3e5adee68bc42c9
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-04-11 21:24:53 +0530

    # merge two list

--- PYTHON/031_file.py
+++ PYTHON/031_file.py
@@ -33,3 +33,62 @@ def merge_sorted_arrays(arr1, arr2):
 nums1 = [1,2,3]
 nums2 = [2,5,6]
 merge_sorted_arrays(nums1, nums2)
+
+
+#V1 leetcode
+class Solution:
+    def removeZero(self, arr):
+        merged = []
+        for num in arr:
+            if num != 0:
+                merged.append(num)
+        print(merged)
+        return merged
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+
+        merged = []
+        h1 = 0
+        h2 = 0
+        while h1 < m and h2 < n:
+            if nums1[h1] < nums2[h2]:
+                merged.append(nums1[h1])
+                h1 += 1
+            else:
+                merged.append(nums2[h2])
+                h2 += 1
+
+        while h1 < m:
+            merged.append(nums1[h1])
+            h1 += 1
+
+        while h2 < n:
+            merged.append(nums2[h2])
+            h2 += 1
+        for i in range(len(merged)):
+            nums1[i] = merged[i]
+
+
+# merge two sorted array [ArrayList] V@
+nums1 = [1,2,3]
+nums2 = [2,5,6]
+
+merged = []
+h1 = 0
+h2 = 0
+while h1 < len(nums1) and h2 < len(nums2):
+    if nums1[h1] < nums2[h2]:
+        merged.append(nums1[h1])
+        h1 += 1
+    else:
+        merged.append(nums2[h2])
+        h2 += 1
+    print(merged)
+
+while h1 < len(nums1):
+    merged.append(nums1[h1])
+    h1 += 1
+
+while h2 < len(nums2):
+    merged.append(nums2[h2])
+    h2 += 1
+print("final: ",merged)
